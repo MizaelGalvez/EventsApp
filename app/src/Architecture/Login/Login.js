c7c411af638e917d6403a590281b1c5cc65ee8d2
@MizaelGalvez <mizaelgalvez@gmail.com>
2018-09-10 00:03:41 -0700

    Pruebas con Estilos Inline
    simple ejemplo para aplicar en otros elementos, se puede aplicar a carruseles y demas.

--- app/src/Architecture/Login/Login.js
+++ app/src/Architecture/Login/Login.js
@@ -2,28 +2,58 @@ import React, { Component } from 'react';
 import logo from './LogoGrande.png';
 import './Login.css';
 
+
+
 class Login extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+        Usuario: null,
+        bool: 1,
+        color: 'purple',
+    }
+  }
+
+  CambiarColorStyles = (event) => {
+    if (this.state.bool == 1) {
+      this.setState({color: 'blue', bool: 0});
+
+    }else {
+      this.setState({color: 'purple', bool: 1});
+    }
+  }
+
   render() {
+
+    const Styles = {
+    color: this.state.color,
+    };
+
     return (
       <div className="Login">
         <header className="Login-header">
           <img src={logo} className="Login-logo" alt="logo" />
           <h1 className="Login-title">Inicia Sesion</h1>
         </header>
-            <div>
-              <input type="text" required placeholder="  Usuario"/>
-            </div>
-            <div>
-              <input type="password" required placeholder="  password"/>
-            </div>
-            <div>
-              <a class="boton_personalizado" >Iniciar Sesion</a>
-            </div>
-            <div>
-              <p class="Login-crearCuenta">o crea tu cuenta<a href=""> aqui </a>!!</p>
-            </div>
-    </div>
 
+        <div>
+          <input type="text" required placeholder="  Usuario"/>
+        </div>
+
+        <div>
+          <input type="password" required placeholder="  password"/>
+        </div>
+
+        <div>
+          <a class="boton_personalizado" onClick={this.CambiarColorStyles} >Iniciar Sesion</a>
+        </div>
+
+        <div>
+          <p class="Login-crearCuenta">o crea tu cuenta<a href="" style={Styles}> aqui </a>!!</p>
+        </div>
+
+    </div>
 
     );
   }
