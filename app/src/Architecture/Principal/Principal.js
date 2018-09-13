cea1c338f2eea99736405cfff55a0d032c4578ff
@MizaelGalvez <mizaelgalvez@gmail.com>
2018-09-13 10:40:29 -0700

    isue cam
    #01

--- app/src/Architecture/Principal/Principal.js
+++ app/src/Architecture/Principal/Principal.js
@@ -1,37 +1,86 @@
-import React, { Component } from 'react'
-import QrReader from 'react-qr-reader'
+import React, { Component } from 'react';
+import Camera from 'react-camera';
+import QrCodeScanner from '@sensorfactdev/qr-code-scanner';
 
-class Principal extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      delay: 300,
-      result: 'No result',
-    }
-    this.handleScan = this.handleScan.bind(this)
+export default class Principal extends Component {
+
+  constructor(props) {
+    super(props);
+    this.takePicture = this.takePicture.bind(this);
   }
-  handleScan(data){
-    if(data){
-      this.setState({
-        result: data,
-      })
-    }
+
+  takePicture() {
+    this.camera.capture()
+    .then(blob => {
+      this.img.src = URL.createObjectURL(blob);
+      this.img.onload = () => { URL.revokeObjectURL(this.src); }
+    })
   }
-  handleError(err){
-    console.error(err)
-  }
-  render(){
-    return(
+  handleScanResult = result => {
+  console.log(result);
+  // {
+  //   "result": "RESULT STRING VALUE",
+  //   "points": [
+  //     {
+  //       "x": 171,
+  //       "y": 445,
+  //       "count": 2,
+  //       "estimatedModuleSize": 3.857142857142857
+  //     },
+  //     // .... more points
+  //   ]
+  // }
+}
+
+  render() {
+    return (
+
+
+      <div style={style.container}>
       <div>
-        <QrReader
-          delay={this.state.delay}
-          onError={this.handleError}
-          onScan={this.handleScan}
-          style={{ width: '100%' }}
-          />
-        <p>{this.state.result}</p>
+   <QrCodeScanner
+     onQrCodeScanned={this.handleScanResult}
+     width={window.innerWidth}
+     height={window.innerHeight}
+     showAimAssist // Defaults to true, disable to remove the aim assist
+   />
+ </div>
+        <Camera
+          style={style.preview}
+          ref={(cam) => {
+            this.camera = cam;
+          }}
+        >
+          <div style={style.captureContainer} onClick={this.takePicture}>
+            <div style={style.captureButton} />
+          </div>
+        </Camera>
       </div>
-    )
+    );
   }
 }
-export default Principal;
+
+const style = {
+  preview: {
+    position: 'relative',
+  },
+  captureContainer: {
+    display: 'flex',
+    position: 'absolute',
+    justifyContent: 'center',
+    zIndex: 1,
+    bottom: 0,
+    width: '100%'
+  },
+  captureButton: {
+    backgroundColor: '#fff',
+    borderRadius: '50%',
+    height: 56,
+    width: 56,
+    color: '#000',
+    margin: 20
+  },
+  captureImage: {
+    width: '100%',
+  }
+};
