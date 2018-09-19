af8a60b7b52dfc863208c194f62dea5014de0013
@MizaelGalvez <mizaelgalvez@gmail.com>
2018-09-19 10:04:54 -0700

    Add view Home

--- app/src/Architecture/Principal/Principal.js
+++ app/src/Architecture/Principal/Principal.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './Principal.css';
+import QRimagen from './QR.png';
 
 class Principal extends Component {
 
@@ -7,7 +8,11 @@ class Principal extends Component {
   render() {
     return (
       <div className='Principal'>
-      <p>Inicio</p>
+      <img src={QRimagen} className="QRimagen" alt="MizaelDevs" />
+      <input className='BotonEscanear' type="file" value="" accept="image/*" capture="camera" label="Escanear"></input>
+      <h3 className='h3'>Registros Asistentes : <p className='Cantidades'>358</p></h3>
+      <h2 className='BottonDatos'>Consultar Datos</h2>
+      <div></div>
       </div>
     );
   }
